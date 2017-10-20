922e7c1b1bdda3c37cf1d78eb84397308ef6794d
Dick Cavender <dcavender@pivotal.io>
2017-10-20 10:40:21 -0700

    Add gcs resource and attempt to install gcloud in build script.
    Signed-off-by: Sean Goller <sgoller@pivotal.io>

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -44,6 +44,19 @@ printf "\nUsing the following JDK:"
 java -version
 printf "\n\n"
 
+apt-get update && apt-get install -y lsb-release
+# Create an environment variable for the correct distribution
+export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+
+# Add the Cloud SDK distribution URI as a package source
+echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+
+# Import the Google Cloud Platform public key
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+
+# Update the package list and install the Cloud SDK
+apt-get update && apt-get install -y google-cloud-sdk
+
 export TERM=${TERM:-dumb}
 export DEST_DIR=$(pwd)/built-geode
 export TMPDIR=${DEST_DIR}/tmp
