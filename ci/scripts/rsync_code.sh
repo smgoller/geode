612db274a7b3e67fdd56ac0336a8874d36e3962a
Jacob Barrett <jbarrett@pivotal.io>
2018-07-19 13:24:10 -0700

    Add rsync_code job.
    Signed-off-by: Sean Goller <sgoller@pivotal.io>

---
+++ ci/scripts/rsync_code.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set +e
+set -x
+
+env
+
+BASE_DIR=$(pwd)
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  SCRIPTDIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+SCRIPTDIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+
+REPODIR=$(cd geode; git rev-parse --show-toplevel)
+
+BUILD_NAME=$(cat concourse-metadata/build-name)
+BUILD_JOB_NAME=$(cat concourse-metadata/build-job-name)
+BUILD_PIPELINE_NAME=$(cat concourse-metadata/build-pipeline-name)
+
+INSTANCE_NAME="$(echo "geode-builder-${BUILD_PIPELINE_NAME}-${BUILD_JOB_NAME}-${BUILD_NAME}" | tr '[:upper:]' '[:lower:]')"
+INSTANCE_IP_ADDRESS=$(gcloud compute instances list  | awk "/^${INSTANCE_NAME}/ {print \$5}")
+PROJECT=apachegeode-ci
+ZONE=us-central1-f
+
+# generate an ssh key for us to use
+gcloud compute --project=${PROJECT} ssh geode@${INSTANCE_NAME} --zone=${ZONE} --quiet -- true
+
+rsync -e "ssh -i $HOME/.ssh/google_compute_engine" -aPzh ${REPODIR} geode@${INSTANCE_IP_ADDRESS}:.
