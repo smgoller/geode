c41ce6622c944bf313fcef2244542cb7edf8b601
Sean Goller <sgoller@pivotal.io>
2018-10-24 16:33:19 -0700

    Fix up clean_fork_pipelines.sh.
    Co-authored-by: Sean Goller <sgoller@pivotal.io>
    Co-authored-by: Dick Cavender <dcavender@pivotal.io>

--- ci/pipelines/meta/clean_fork_pipelines.sh
+++ ci/pipelines/meta/clean_fork_pipelines.sh
@@ -22,12 +22,42 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
 done
 SCRIPTDIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-TARGET=geode
-GEODE_FORK=${1}
+META_PROPERTIES=${SCRIPTDIR}/meta.properties
+LOCAL_META_PROPERTIES=${SCRIPTDIR}/meta.properties.local
+
+## Load default properties file
+source ${META_PROPERTIES}
+echo "**************************************************"
+echo "Default Environment variables for this deployment:"
+cat ${SCRIPTDIR}/meta.properties | grep -v "^#"
+echo "**************************************************"
+
+## Load local overrides properties file
+if [[ -f ${LOCAL_META_PROPERTIES} ]]; then
+  echo "Locally overridden environment variables for this:"
+  cat ${SCRIPTDIR}/meta.properties.local
+  echo "**************************************************"
+  source ${LOCAL_META_PROPERTIES}
+fi
+
+source ${META_PROPERTIES}
+
+if [[ -f ${LOCAL_META_PROPERTIES} ]]; then
+  source ${LOCAL_META_PROPERTIES}
+fi
+
+read -n 1 -s -r -p "Press any key to continue or x to abort" DEPLOY
+echo
+if [[ "${DEPLOY}" == "x" ]]; then
+  echo "x pressed, aborting deploy."
+  exit 0
+fi
+
+TARGET=${CONCOURSE_HOST}
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 GEODE_BRANCH=${2:-${CURRENT_BRANCH}}
 
-. ${SCRIPTDIR}/shared/utilities.sh
+. ${SCRIPTDIR}/../shared/utilities.sh
 SANITIZED_GEODE_BRANCH=$(getSanitizedBranch ${GEODE_BRANCH})
 SANITIZED_GEODE_FORK=$(getSanitizedFork ${GEODE_FORK})
 
@@ -43,23 +73,36 @@ fi
 
 echo "Fork is ${GEODE_FORK}"
 echo "Branch is ${GEODE_BRANCH}"
+OLD_GCP_PROJECT=$(gcloud config get-value project)
+echo "Setting project from ${OLD_GCP_PROJECT} to ${GCP_PROJECT}"
+gcloud config set project ${GCP_PROJECT}
 
 echo "Deleting meta pipeline if it exists..."
 META_PIPELINE="${SANITIZED_GEODE_FORK}-${SANITIZED_GEODE_BRANCH}-meta"
-fly -t ${TARGET} destroy-pipeline --non-interactive -p ${META_PIPELINE}
+fly -t ${TARGET} destroy-pipeline -n -p ${META_PIPELINE}
 
 echo "Deleting images pipeline if it exists..."
 IMAGES_PIPELINE="${SANITIZED_GEODE_FORK}-${SANITIZED_GEODE_BRANCH}-images"
-fly -t ${TARGET} destroy-pipeline --non-interactive -p ${IMAGES_PIPELINE}
+fly -t ${TARGET} destroy-pipeline -n -p ${IMAGES_PIPELINE}
 
 echo "Deleting reaper pipeline if it exists..."
 REAPER_PIPELINE="${SANITIZED_GEODE_FORK}-${SANITIZED_GEODE_BRANCH}-reaper"
-fly -t ${TARGET} destroy-pipeline --non-interactive -p ${REAPER_PIPELINE}
+fly -t ${TARGET} destroy-pipeline -n -p ${REAPER_PIPELINE}
 
 echo "Deleting build pipeline if it exists..."
 BUILD_PIPELINE="${SANITIZED_GEODE_FORK}-${SANITIZED_GEODE_BRANCH}-main"
-fly -t ${TARGET} destroy-pipeline --non-interactive -p ${BUILD_PIPELINE}
+fly -t ${TARGET} destroy-pipeline -n -p ${BUILD_PIPELINE}
 
+echo "Deleting examples pipeline if it exists..."
+EXAMPLES_PIPELINE="${SANITIZED_GEODE_FORK}-${SANITIZED_GEODE_BRANCH}-examples"
+fly -t ${TARGET} destroy-pipeline -n -p ${EXAMPLES_PIPELINE}
+
+echo "Deleting metrics pipeline if it exists..."
+METRICS_PIPELINE="${SANITIZED_GEODE_FORK}-${SANITIZED_GEODE_BRANCH}-metrics"
+fly -t ${TARGET} destroy-pipeline -n -p ${METRICS_PIPELINE}
+
+
+set -x
 gcloud container images list | grep "${SANITIZED_GEODE_FORK}-${SANITIZED_GEODE_BRANCH}" | while IFS= read -r line; do
   echo "Deleting image: ${line}"
   gcloud container images delete ${line}:latest --quiet
@@ -74,3 +117,7 @@ gcloud compute images list | awk "/^${SANITIZED_GEODE_FORK}-${SANITIZED_GEODE_BR
   gcloud compute images delete ${line} --quiet
 done
 
+if [[ ! -z "${OLD_GCP_PROJECT}" ]]; then
+  echo "Restoring GCP Project to ${OLD_GCP_PROJECT}"
+  gcloud config set project ${OLD_GCP_PROJECT}
+fi
